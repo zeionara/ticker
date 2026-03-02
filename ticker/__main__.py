1f6b6e2f1689975d1e803b75757d7d5b292c2a3b
Zeio Nara <zeionara@gmail.com>
2026-03-03 00:04:35 +0300

    feat(doctrinaetnobiles): added ticket tracker for doctrina et nobiles website

--- ticker/__main__.py
+++ ticker/__main__.py
@@ -1,20 +1,28 @@
+import asyncio
+from os import getenv
 from time import sleep
 from datetime import datetime
-from os import getenv
-import asyncio
+from logging import INFO, basicConfig, getLogger
 
+from requests import get
+from bs4 import BeautifulSoup
 from click import group, argument, option
-
-from requests import get, post
 from telegram.ext import Application, ApplicationBuilder
 
 
-INTERVAL = 20  # seconds
+INTERVAL = 30  # seconds
+TIMEOUT = 300  # seconds
 
 TG_BOT_TOKEN = getenv('TG_BOT_TOKEN')
 TG_CHAT_ID = int(getenv('TG_CHAT_ID'))
 
-URL = 'https://tickets.hermitagemuseum.org/event/{hash_id}'
+URL = 'https://doctrinaetnobiles.ru/lectures/{label}/'
+
+logger = getLogger(__name__)
+basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    level=INFO
+)
 
 
 @group()
@@ -22,11 +30,11 @@ def main():
     pass
 
 
-def notify(app: Application, hash_id: str):
-    async def send_message():
-        await app.bot.sendMessage(TG_CHAT_ID, 'The tickets are released!\n\n' + URL.format(hash_id = hash_id))
+def send_message(app: Application, message: str):
+    async def send_message_async():
+        await app.bot.sendMessage(TG_CHAT_ID, message)
 
-    asyncio.run(send_message())
+    asyncio.run(send_message_async())
 
 
 def now():
@@ -34,40 +42,35 @@ def now():
 
 
 @main.command()
-@argument('url', type = str, default = 'https://tickets.hermitagemuseum.org/api/afisha')
-@option('--insecure', '-i', is_flag = True)
+@argument('label', type = str)
 @option('--interval', '-t', type = int, default = INTERVAL)
-def track(url: str, insecure: bool, interval: int):
+def track(label: str, interval: int):
     app = ApplicationBuilder().token(TG_BOT_TOKEN).build()
 
+    url = URL.format(label = label)
+
     while True:
-        page = get(url, verify = not insecure)
+        try:
+            response = get(url, timeout=TIMEOUT)
+        except Exception as e:
+            send_message(app, f'Failed to load page ({e})\n\n{url}')
+            break
 
-        # response = loads(page.text.encode('latin-1').decode('unicode-escape'))
-        response = page.json()
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'lxml')
 
-        for action in response['response']['action']:
-            description = action.get('descript_ru')
+            book_button = soup.find('p', class_='afdutton')
 
-            if description is not None and description.startswith('Бесплатный'):
-                hash_id = action.get('hash_id')
+            if book_button is None:
+                logger.info('The tickets for event %s are not available yet. Waiting for %d seconds before checking again...', label, interval)
+                sleep(interval)
+                continue
 
-                page = post('https://tickets.hermitagemuseum.org/api/no-scheme', json = {'hash': hash_id}, verify = not insecure)
-
-                response = page.json()
-
-                if response['response']['action']:
-                    notify(app, hash_id)
-
-                    print(f'{now()} The tickets are available @ {URL.format(hash_id = hash_id)}. Notification has been sent.')
-                    return
-                else:
-                    print(f'{now()} The tickets are not available @ {URL.format(hash_id = hash_id)}. Retrying in {interval} seconds.')
-                    # print(response)
-                    # print()
-
-                    sleep(interval)
-                    break
+            send_message(app, f'The tickets are released!\n\n{url}')
+            break
+        else:
+            send_message(app, f'Unexpected response status code ({response.status_code})\n\n{url}')
+            break
 
 
 if __name__ == '__main__':
